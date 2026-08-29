99e07190d0a462d6e62f0190569e859379438f1c
yhbwntdhzd-beep <yhbwntdhzd@privaterelay.appleid.com>
2026-08-29 18:01:11 +0900

    Update login.js

--- login.js
+++ login.js
@@ -1,47 +1,42 @@
 //=============================
-// 初回だけ管理者を作成
-//=============================
-
-let users = JSON.parse(localStorage.getItem("users"));
-
-if(users == null){
-
-    users = [
-
-        {
-            id:"admin",
-            password:"admin",
-            name:"管理者"
-        }
-
-    ];
-
-    localStorage.setItem("users",JSON.stringify(users));
-
-}
-
-//=============================
 // ログイン
 //=============================
 
 function login(){
 
-    const id = document.getElementById("employeeId").value;
+    const id = document.getElementById("employeeId").value.trim();
     const password = document.getElementById("password").value;
 
-    users = JSON.parse(localStorage.getItem("users"));
+    // Firebaseから取得した社員情報を確認
+    const users = JSON.parse(localStorage.getItem("users")) || [];
 
+    // ログインする人を探す
     const user = users.find(function(item){
 
-        return item.id===id && item.password===password;
+        return item.id === id && item.password === password;
 
     });
 
+    // ログイン成功
     if(user){
 
-        localStorage.setItem("loginUser",JSON.stringify(user));
+        // ログイン中のユーザーを保存
+        localStorage.setItem(
+            "loginUser",
+            JSON.stringify(user)
+        );
 
-        location.href="index.html";
+        // 管理者の場合
+        if(user.id === "admin"){
+
+            location.href = "admin.html";
+
+        }else{
+
+            // 社員の場合
+            location.href = "index.html";
+
+        }
 
     }else{
 
