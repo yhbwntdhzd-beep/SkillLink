0ed28428d1600a3254365b07122fd46f041069ac
yhbwntdhzd-beep <yhbwntdhzd@privaterelay.appleid.com>
2026-08-29 17:30:12 +0900

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,64 +1,100 @@
-//=============================
+//================================
+// Firebase
+//================================
+
+import { db } from "./firebase.js";
+
+import {
+    doc,
+    setDoc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+
+//================================
 // 社員追加
-//=============================
+//================================
 
-function addUser(){
+async function addUser(){
 
-    // 入力値取得
+    // 入力値を取得
     const id = document.getElementById("newId").value.trim();
+
     const name = document.getElementById("newName").value.trim();
+
     const password = document.getElementById("newPassword").value.trim();
+
 
     // 入力チェック
     if(id === "" || name === "" || password === ""){
 
-        alert("すべて入力してください。");
+        alert("社員番号・名前・パスワードをすべて入力してください。");
+
         return;
 
     }
 
-    // 保存済み社員取得
-    let users = JSON.parse(localStorage.getItem("users"));
 
-    if(users == null){
+    try{
 
-        users = [];
+        // Firestoreのusersから社員番号を確認
+        const userRef = doc(db, "users", id);
+
+        const snapshot = await getDoc(userRef);
+
+
+        // すでに存在する場合
+        if(snapshot.exists()){
+
+            alert("この社員番号はすでに登録されています。");
+
+            return;
+
+        }
+
+
+        // Firebaseへ社員情報を保存
+        await setDoc(userRef, {
+
+            id: id,
+
+            name: name,
+
+            password: password,
+
+            point: 0
+
+        });
+
+
+        // 登録成功
+        alert("社員を登録しました！");
+
+
+        // 入力欄を空にする
+        document.getElementById("newId").value = "";
+
+        document.getElementById("newName").value = "";
+
+        document.getElementById("newPassword").value = "";
+
+
+    }catch(error){
+
+        console.error("社員登録エラー:", error);
+
+        alert("社員登録に失敗しました。\nConsoleを確認してください。");
 
     }
-
-    // 社員番号重複チェック
-    const exists = users.find(function(user){
-
-        return user.id === id;
-
-    });
-
-    if(exists){
-
-        alert("この社員番号は既に登録されています。");
-        return;
-
-    }
-
-    // 新しい社員
-    const newUser = {
-
-        id: id,
-        name: name,
-        password: password
-
-    };
-
-    users.push(newUser);
-
-    // 保存
-    localStorage.setItem("users", JSON.stringify(users));
-
-    alert("社員を登録しました！");
-
-    // 入力欄クリア
-    document.getElementById("newId").value="";
-    document.getElementById("newName").value="";
-    document.getElementById("newPassword").value="";
 
 }
+
+
+//================================
+// ボタンに登録処理を設定
+//================================
+
+document.getElementById("addUserButton").addEventListener(
+    "click",
+    addUser
+);
